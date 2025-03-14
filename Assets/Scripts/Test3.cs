7cff52f8103c0e587edf088f56b362d628a423f9
cha-mung <cha7367@naver.com>
2025-03-15 00:33:32 +0900

    test3

--- Assets/Scripts/Test3.cs
+++ Assets/Scripts/Test3.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Test3 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("hellooo");
+        
     }
 
     // Update is called once per frame
